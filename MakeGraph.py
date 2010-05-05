3b90a7b9e7bb3f73419c8824a84d015b3d964103
Ben Ballard <bxb107@case.edu>
2010-05-05 19:45:57 -0400

    Fixed some outputs

--- MakeGraph.py
+++ MakeGraph.py
@@ -13,31 +13,19 @@ print "Graphing...."
 
 f = loadtxt(fileInputname, delimiter=',')	
 
-print f
-
+x,y = f.shape
 
 fig = pylab.figure()
 ax = Axes3D(fig)
 
-x,y = f.shape
-
-print x
-print y
 
 xArray =arange(x).reshape(x,1)
-print xArray
 for r in range(y-1):
 	xArray = hstack((xArray,arange(x).reshape(x,1)))	
 
 yArray = arange(y)
 for r in range(x-1):
 	yArray = vstack((yArray,arange(y)))	
-print xArray
-print yArray
-
-print xArray.shape
-print yArray.shape
-print f.shape
 ax.plot_surface(xArray,yArray,f,rstride=5,cstride=5,cmap=cm.jet)
 
 #ax.plot_surface(xArray,yArray,f,cmap=cm.jet)
